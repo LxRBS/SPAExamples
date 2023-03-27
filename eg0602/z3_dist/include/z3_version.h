18a7f8fe58574e3ae367c5c63f7a16416a2ae613
luoxun <luoxun@tsinghua.org.cn>
2023-03-27 15:23:18 +0800

    所有z3都引自eg0601

--- eg0602/z3_dist/include/z3_version.h
+++
@@ -1,8 +0,0 @@
-// automatically generated file.
-#define Z3_MAJOR_VERSION   4
-#define Z3_MINOR_VERSION   8
-#define Z3_BUILD_NUMBER    10
-#define Z3_REVISION_NUMBER 0
-
-#define Z3_FULL_VERSION    "4.8.10.0 517d907567f4283ad8b48ff9c2a3f6dce838569e z3-4.8.4-4541-g517d90756"
-#define Z3GITHASH 517d907567f4283ad8b48ff9c2a3f6dce838569e
